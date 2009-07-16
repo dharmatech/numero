b0c1041cef2d527077f8632dfb2ee4552106d960
Eduardo Cavazos <wayo.cavazos@gmail.com>
2009-07-16 02:26:48 -0500

    Update examples

--- symbolic/examples.scm
+++ symbolic/examples.scm
@@ -11,7 +11,7 @@
 
 (to-alg
  (simplify
-  (infix '(a + b + 2 * a))))
+  (infix '(a + b + 2 a))))
 
 "3 * a + b"
 
@@ -23,19 +23,19 @@
  (simplify
   (D (infix '( 7 x ^ 5 - 3 x ^ 4 + 6 x ^ 2 + 3 x + 4 )) 'x)))
 
-"35 * x ^ 4 + -12 * x ^ 3 + 12 * x + 3"
+"3 + 35 * x ^ 4 - 12 * x ^ 3 + 12 * x"
 
 (to-alg
  (simplify
   (D (infix '( ( 3 x - 2 ) / ( x ^ 2 + 7 ) )) 'x)))
 
-"(3 * x ^ 2 + 21 - 2 * x * (3 * x + -2)) / (x ^ 2 + 7) ^ 2"
+"(3 * x ^ 2 + 21 - 2 * x * (3 * x - 2)) / (x ^ 2 + 7) ^ 2"
 
 (to-alg
  (simplify
   (D (infix '( (3 x - 2) / (2 x + 5) )) 'x)))
 
-"(6 * x + 15 + -6 * x + 4) / (2 * x + 5) ^ 2"
+"19 / (2 * x + 5) ^ 2"
 
 ;; Example from Mathematica reference manual
 
