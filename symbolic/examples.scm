bb7b88d5ca9282b942e41d606da58faa98c62c37
Eduardo Cavazos <wayo.cavazos@gmail.com>
2009-07-19 13:16:30 -0500

    examples: Strang 4.2 example 1

--- symbolic/examples.scm
+++ symbolic/examples.scm
@@ -3,7 +3,9 @@
         (numero symbolic to-alg)
         (numero symbolic simplify)
         (numero symbolic derivative)
-        (numero symbolic expand))
+        (numero symbolic expand)
+        (numero symbolic solve-for)
+        (numero symbolic subst))
 
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; simplify
@@ -33,7 +35,8 @@
 
 (to-alg
  (simplify
-  (D (infix '( (3 x - 2) / (2 x + 5) )) 'x)))
+  (simplify
+   (D (infix '( (3 x - 2) / (2 x + 5) )) 'x))))
 
 "19 / (2 * x + 5) ^ 2"
 
@@ -41,19 +44,19 @@
 
 (to-alg
  (simplify
-  (expand-loop
+  (expand
    (simplify
-    (expand-loop
-     (D (D (D (D (infix '(sin(x) ^ 10)) 'x) 'x) 'x) 'x))))))
+    (D (D (D (D (infix '(sin(x) ^ 10)) 'x) 'x) 'x) 'x)))))
 
-"-4680 * (cos x) ^ 2 * (sin x) ^ 8 + 5040 * (cos x) ^ 4 * (sin x) ^ 6 + 280 * (sin x) ^ 10"
+"-4680 * (cos x) ^ 2 * (sin x) ^ 8 + 280 * (sin x) ^ 10 + 5040 * (cos x) ^ 4 * (sin x) ^ 6"
 
 ;; 8.41 Mendelson
 
 (to-alg
  (simplify
-  (expand-loop
-   (D (infix '(x * (2 x - 1) * (x + 2))) 'x))))
+  (expand
+   (simplify
+    (D (infix '(x * (2 x - 1) * (x + 2))) 'x)))))
 
 "6 * x ^ 2 + 6 * x - 2"
 
@@ -61,10 +64,10 @@
 
 (to-alg
  (simplify
-  (expand-loop
+  (expand
    (D (infix '( (x ^ 2 - 3) / (x + 4) )) 'x))))
 
-"(x ^ 2 + 8 * x + 3) / (8 * x + x ^ 2 + 16)"
+"(3 + x ^ 2 + 8 * x) / (8 * x + x ^ 2 + 16)"
 
 ;; 8.50 Mendelson
 
@@ -72,15 +75,100 @@
  (simplify
   (D (infix '( (x ^ 5 - x + 2) / (x ^ 3 + 7) )) 'x)))
 
-"((5 * x ^ 4 - 1) * (x ^ 3 + 7) - 3 * x ^ 2 * (x ^ 5 - 1 * x + 2)) / (x ^ 3 + 7) ^ 2"
+"  ((5 * x ^ 4 - 1) * (x ^ 3 + 7) - 3 * x ^ 2 * (x ^ 5 - x + 2)) /
+   (x ^ 3 + 7) ^ 2  "
 
 ;; 8.51 Mendelson
 
 (to-alg
  (simplify
-  (expand-loop
-   (simplify
-    (expand-loop
+  (simplify
+   (expand
+    (simplify
      (D (infix '( (3 x ^ 7 + x ^ 5 - 2 x ^ 4 + x - 3) / x ^ 4 )) 'x))))))
 
-"9 * x ^ 2 + 1 - 3 * x ^ -4 + 12 * x ^ -5"
+"-3 / x ^ 4 + 9 * x ^ 2 + 1 + 12 / x ^ 5"
+
+;; 9.7
+
+(to-alg
+ (simplify
+  (D (infix '(1 / (3 x ^ 2 + 5) ^ 4)) 'x)))
+
+"-24 * x / (3 * x ^ 2 + 5) ^ 5"
+
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(to-alg
+ (simplify
+  (simplify
+   (expand
+    (infix '((a - b) ^ 3))))))
+
+"-3 * a ^ 2 * b + 3 * a * b ^ 2 + a ^ 3 - b ^ 3"
+
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; solve-for
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(solve-for (infix '(2 x = 6)) 'x)
+
+(= x (/ 6 2))
+
+(solve-for (infix '(a b c / d = e)) 'c)
+
+(= c (/ (* d e) (* a b)))
+
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Calculus - Gilbert Strang
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; 4.2 Example 1 (implicit differentiation)
+
+;; y^5 + x y = 3
+
+;; Find dy/dx at x = 2 and y = 1
+
+;; Define the equation:
+
+(define eq-1 (infix '( y(x) ^ 5 + x y(x) = 3 )))
+
+;; Take the derivative of the whole equation:
+
+(set! eq-2 (simplify (D eq-1 'x)))
+
+(to-alg eq-2)
+
+"5 * (D (y x) x) * (y x) ^ 4 + (y x) + x * (D (y x) x) = 0"
+
+;; Let's use 'subst' to make it prettier:
+
+(set! eq-3
+      (subst eq-2
+             ((D (y x) x) 'dy/dx)
+             ((y x) 'y)))
+
+(to-alg eq-3)
+
+"5 * dy/dx * y ^ 4 + y + x * dy/dx = 0"
+
+;; Plug in x=2 and y=1 :
+
+(set! eq-4
+      (simplify
+       (subst eq-3
+              (x 2)
+              (y 1))))
+
+(to-alg eq-4)
+
+"7 * dy/dx + 1 = 0"
+
+;; Solve for dy/dx:
+
+(to-alg
+ (simplify
+  (solve-for eq-4 'dy/dx)))
+
+"dy/dx = -1/7"
+
